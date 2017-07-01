7b9336a13483aabe3a7718d78d736c5497ca3a48
Nicolas Hohm <nickel7152@gmail.com>
2017-07-01 21:19:53 +0200

    Build all the stuff with callbacks

--- main.js
+++ main.js
@@ -3,27 +3,39 @@ var Table = require('cli-table');
 
 var input = 'Я всё детство о ней мечтал';
 var wordsToTranslate = input.split(' ');
-var translation = {};
+var translation = [];
 
-wordsToTranslate.map(function(val) {
-translation[val] = '';
-translate(val, {from: 'ru', to: 'de'}).then(res => {
-    //console.log(val);
-    //console.log(res.text);
-    translation[val] = res.text;
-}).catch(err => {
-    console.error(err);
-});
+//for (var i = 0; i < myArray.length; i++)
 
-});
+var i = 0;
 
-var table = new Table({
-  chars: { 'top': '' , 'top-mid': '' , 'top-left': '' , 'top-right': ''
-         , 'bottom': '' , 'bottom-mid': '' , 'bottom-left': '' , 'bottom-right': ''
-         , 'left': '' , 'left-mid': '' , 'mid': '' , 'mid-mid': ''
-         , 'right': '' , 'right-mid': '' , 'middle': ' ' },
-  style: { 'padding-left': 0, 'padding-right': 0 }
-});
-console.log(translation);
-table.push(wordsToTranslate, translation);
-console.log(table.toString());
+doTranslation();
+
+function doTranslation() {
+    translate(wordsToTranslate[i], {from: 'ru', to: 'de'}).then(res => {
+        console.log(res.text);
+        translation.push(res.text);
+        console.log('debug');
+        i++;
+        if (wordsToTranslate.length > i) {
+            doTranslation();
+        } else {
+            finish();
+        }
+    }).catch(err => {
+        console.error(err);
+    });
+}
+
+function finish() {
+    var table = new Table({
+      chars: { 'top': '' , 'top-mid': '' , 'top-left': '' , 'top-right': ''
+             , 'bottom': '' , 'bottom-mid': '' , 'bottom-left': '' , 'bottom-right': ''
+             , 'left': '' , 'left-mid': '' , 'mid': '' , 'mid-mid': ''
+             , 'right': '' , 'right-mid': '' , 'middle': ' ' },
+      style: { 'padding-left': 0, 'padding-right': 0 }
+    });
+    console.log(translation);
+    table.push(wordsToTranslate, translation);
+    console.log(table.toString());
+}
